5ad32eb51ad5057e230262784c664fb8878712db
chiwala4dad <chiwala4dad@gmail.com>
2018-06-13 23:31:35 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require("fs");
+const audioFiles = fs.readdirSync("./data");
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -9,7 +11,14 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('fuck me pussy boy');
-  	}
+  	} else if (message.content === 'pp') {
+         voiceChannel.join().then(connection => {
+
+        connection.playFile("./data/" + randAudioFile).on("end", () => {
+            connection.disconnect();
+            voiceChannel.leave();
+        });
+
 });
 
 // THIS  MUST  BE  THIS  WAY
