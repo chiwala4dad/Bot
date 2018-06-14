5c9f7e3b8b6c61d139479742160d01bb9eab07c1
chiwala4dad <chiwala4dad@gmail.com>
2018-06-13 22:37:07 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require("fs");
+const _ = require("underscore");
+const moment = require("moment");
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -34,6 +37,6 @@ client.on('message', message => {
        }
    }
 });
-});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
