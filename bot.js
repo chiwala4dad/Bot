c05c41a5da849905cf063c78e0755ab0d1b0f563
chiwala4dad <chiwala4dad@gmail.com>
2018-06-13 23:13:49 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require("fs");
-const _ = require("underscore");
-const moment = require("moment");
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -10,32 +8,8 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.reply('boi if you dont get the fuck outta here with that ping bullshit');
+    	message.reply('pong');
   	}
-    else if (message.content === "pp.") {
-
-       // make sure the user is in a voice channel
-       if (message.member.voiceChannel) {
-
-           // join the user's voice channel
-           message.member.voiceChannel.join()
-               .then(connection => {
-
-                   // get a random audio file from our list
-                   const randAudioFile = _.sample(audioFiles);
-
-                   // play the random audio file
-                   const dispatcher = connection.playFile("./data/" + randAudioFile);
-
-                   // disconnect from the voice channel when the quote is over
-                   dispatcher.on("end", () => {
-                       message.member.voiceChannel.leave();
-                       fs.appendFileSync("log.txt", moment().format("YYYY-MM-DD HH:mm:ss.SSS ") + randAudioFile + "\n");
-                   });
-               })
-               .catch(console.log);
-       }
-   }
 });
 
 // THIS  MUST  BE  THIS  WAY
